02779998b0580d57913e2728fc37fab566c20dac
Brian Calvert <bmcalvert10@gmail.com>
2017-05-03 22:24:21 -0700

    API changes for plotting Multi-Dimensional Shape Spectra

--- HeaderFiles/StopPlotShapeSetup.h
+++ HeaderFiles/StopPlotShapeSetup.h
@@ -1,4 +1,4 @@
-//Similar to the functions used in StopPlotSetup, except for running using the shape nTuples (i.e. for backup plots related to the MT2 shape analysis
+//Similar to the functions used in StopPlotSetup, except for running using the shape nTuples (i.e. for backup plots related to the MT2 shape analysis)
 
 void SetupPlotRunning_ShapesPart1(int argc, char * argv[], RunParams * inRP, PlotMakingStructs * inPMS, AncillaryDrawingVariables * inADV, GlobalHistPlotInfo * inGHPI, HistDisplayParams * inHDP) {
     inRP->DefaultVarVals();
@@ -18,18 +18,19 @@ void SetupPlotRunning_ShapesPart1(int argc, char * argv[], RunParams * inRP, Plo
     TString stringMETExtra = inRP->SLS.noType0 ? " w/o Type 0" : "";
     TString genCutUsed = "";
     inGHPI->DefaultVarVals();
+    inGHPI->SetParamsFromCommandLine(argc, argv);
     inGHPI->SetFracRatioADPNameRange(inRP->GHS.doAbsRatio, inRP->GHS.ReturnFracRatioBound(1), inRP->GHS.ReturnFracRatioBound(2));
     inGHPI->SetLatexStringVec(19656, typeMET, stringMETExtra, inRP->SLS.whichDilepType, genCutUsed, true);    
 }
 
 
-void SetupPlotRunning_DataMCShape(RunParams * inRP, PlotMakingStructs * inPMS, HistogramDisplayStructs * inHDS_Data, HistogramDisplayStructs * inHDS_MC, bool doVerb = false) {
+void SetupPlotRunning_DataMCShape(RunParams * inRP, PlotMakingStructs * inPMS, HistogramDisplayStructs * inHDS_Data, HistogramDisplayStructs * inHDS_MC, TString addString, bool doVerb = false) {
     bool isNotSig = false;
     vector<TString> vecBaseFileNames_Data;
     StopBaseFileNames_DataShape(&vecBaseFileNames_Data);
     
     inHDS_Data->DefaultVarVals();
-    inHDS_Data->SetInputFileShape(&vecBaseFileNames_Data, inRP->SLS.FullPathFile(0), inRP->SLS.ShapeNameAppend(), 0, doVerb);
+    inHDS_Data->SetInputFileShape(&vecBaseFileNames_Data, inRP->SLS.FullPathFile(0), addString + inRP->SLS.ShapeNameAppend(), 0, doVerb);
     inHDS_Data->SetCompParams("Data", 0);
     inHDS_Data->SetSystSize(0);
     
@@ -37,7 +38,7 @@ void SetupPlotRunning_DataMCShape(RunParams * inRP, PlotMakingStructs * inPMS, H
     StopBaseFileNames_MCShape(&vecBaseFileNames_MC);
     
     inHDS_MC->DefaultVarVals();
-    inHDS_MC->SetInputFileShape(&vecBaseFileNames_MC, inRP->SLS.FullPathFile(1), inRP->SLS.ShapeNameAppend(), inPMS->vecSystNames.size(), doVerb);
+    inHDS_MC->SetInputFileShape(&vecBaseFileNames_MC, inRP->SLS.FullPathFile(1), addString + inRP->SLS.ShapeNameAppend(), inPMS->vecSystNames.size(), doVerb);
     inHDS_MC->SetCompParams("MC", 1);
     
     inHDS_MC->SetIndMCParams(&inPMS->vecMCLegends, &inPMS->vecMCColors, &inPMS->vecMCSortParams);
@@ -45,14 +46,43 @@ void SetupPlotRunning_DataMCShape(RunParams * inRP, PlotMakingStructs * inPMS, H
     SetSystBasicsShape(inHDS_MC, &inPMS->vecSystNames);
 }
 
-void SetupPlotRunning_SignalShape(RunParams * inRP, PlotMakingStructs * inPMS, HistogramDisplayStructs * inHDS_Signal, bool doVerb = false) {
+void SetupPlotRunning_SignalShapeSpecial(RunParams * inRP, PlotMakingStructs * inPMS, HistogramDisplayStructs * inHDS_Signal, TString signalString, TString addString, bool doVerb = false) {
     
     vector<TString> vecBaseFileNames_Signal;
-    StopBaseFileNames_SignalShape(&vecBaseFileNames_Signal, &inRP->SLS, doVerb);
     
+    
+    if (signalString.Length() > 0) {
+        vecBaseFileNames_Signal.push_back(signalString);
+    }
+    else {
+        StopBaseFileNames_SignalShape(&vecBaseFileNames_Signal, &inRP->SLS, doVerb);
+    }
     
     inHDS_Signal->DefaultVarVals();
-    inHDS_Signal->SetInputFileShape(&vecBaseFileNames_Signal, inRP->SLS.FullPathFile(2), inRP->SLS.ShapeNameAppend(), inPMS->vecSystNames.size(), doVerb);
+    inHDS_Signal->SetInputFileShape(&vecBaseFileNames_Signal, "", addString + TString(".root"), inPMS->vecSystNames.size(), doVerb);
+    
+    inHDS_Signal->SetCompParams(inRP->SLS.SignalLegendString(0), -1);
+    inHDS_Signal->SetSystSize(inPMS->numSysts, inRP->SLS.doSyst);
+    SetSystBasics(inHDS_Signal, &inPMS->vecSystNames, inRP->SLS.SmearedPlots, true, doVerb);
+}
+
+void SetupPlotRunning_SignalShape(RunParams * inRP, PlotMakingStructs * inPMS, HistogramDisplayStructs * inHDS_Signal, TString signalString, TString addString, bool doVerb = false) {
+    
+    vector<TString> vecBaseFileNames_Signal;
+    
+    
+    TString additionalString = addString.Contains("WithSigContam") ? "_WithSigContam" : "_NoSigContam";
+    TString grabStringSignal = inRP->SLS.ShapeNameAppend();
+    grabStringSignal.Replace(grabStringSignal.Index(".root"), 5, additionalString + TString(".root"));
+    if (signalString.Length() > 0) {
+        vecBaseFileNames_Signal.push_back(signalString);
+    }
+    else {
+        StopBaseFileNames_SignalShape(&vecBaseFileNames_Signal, &inRP->SLS, doVerb);
+    }
+    
+    inHDS_Signal->DefaultVarVals();
+    inHDS_Signal->SetInputFileShape(&vecBaseFileNames_Signal, inRP->SLS.FullPathFile(2), addString + grabStringSignal, inPMS->vecSystNames.size(), doVerb);
     
     inHDS_Signal->SetCompParams(inRP->SLS.SignalLegendString(0), -1);
     inHDS_Signal->SetSystSize(inPMS->numSysts, inRP->SLS.doSyst);
