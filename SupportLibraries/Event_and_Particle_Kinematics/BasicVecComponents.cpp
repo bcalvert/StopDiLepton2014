7e08ce992a8546704163c57438ed61d68540e8a5
Brian Calvert <bmcalvert10@gmail.com>
2017-06-21 02:27:33 -0700

    Updated BasicVecComponents

--- SupportLibraries/Event_and_Particle_Kinematics/BasicVecComponents.cpp
+++ SupportLibraries/Event_and_Particle_Kinematics/BasicVecComponents.cpp
@@ -24,35 +24,69 @@ BasicVecComponents::BasicVecComponents(TLorentzVector * pVecP4)
 }
 
 void BasicVecComponents::UpdateParams(TLorentzVector * pVecP4) {
-    m_fPt    = pVecP4->Pt();
-    if (m_fPt == 0) {
-        m_fEta = 1E9;
-    }
-    else {
-        m_fEta   = pVecP4->Eta();   
-    }
-    m_fPhi   = pVecP4->Phi();
-    m_fMass  = pVecP4->M();
-    m_fPx    = pVecP4->Px();
-    m_fPy    = pVecP4->Py();
-    m_fPz    = pVecP4->Pz();
-    m_fEn    = pVecP4->E();
+	m_fPt    = pVecP4->Pt();
+	if (m_fPt == 0) {
+		m_fEta = 1E9;
+	}
+	else {
+		m_fEta   = pVecP4->Eta();   
+	}
+	m_fPhi   = pVecP4->Phi();
+	m_fMass  = pVecP4->M();
+	m_fPx    = pVecP4->Px();
+	m_fPy    = pVecP4->Py();
+	m_fPz    = pVecP4->Pz();
+	m_fEn    = pVecP4->E();
 }
 
 void BasicVecComponents::PrintVals(bool bPrintCartesian) {
-    if (bPrintCartesian) {
-        std::cout << "m_fPt " << m_fPt << std::endl;
-        std::cout << "m_fEta " << m_fEta << std::endl;
-        std::cout << "m_fPhi " << m_fPhi << std::endl;
-        std::cout << "m_fMass " << m_fMass << std::endl;
-    }
-    else {
-        std::cout << "m_fPx " << m_fPx << std::endl;
-        std::cout << "m_fPy " << m_fPy << std::endl;
-        std::cout << "m_fPz " << m_fPz << std::endl;
-        std::cout << "m_fEn " << m_fEn << std::endl;
-    }
+	if (bPrintCartesian) {
+		std::cout << "m_fPt " << m_fPt << std::endl;
+		std::cout << "m_fEta " << m_fEta << std::endl;
+		std::cout << "m_fPhi " << m_fPhi << std::endl;
+		std::cout << "m_fMass " << m_fMass << std::endl;
+	}
+	else {
+		std::cout << "m_fPx " << m_fPx << std::endl;
+		std::cout << "m_fPy " << m_fPy << std::endl;
+		std::cout << "m_fPz " << m_fPz << std::endl;
+		std::cout << "m_fEn " << m_fEn << std::endl;
+	}
 }
 void BasicVecComponents::SetP4Vals(TLorentzVector * pVecP4) {
-    pVecP4->SetPxPyPzE(m_fPx, m_fPy, m_fPz, m_fEn);
+	pVecP4->SetPxPyPzE(m_fPx, m_fPy, m_fPz, m_fEn);
 }
+
+float BasicVecComponents::GetPt() const
+{
+	return m_fPt;
+}
+float BasicVecComponents::GetEta() const
+{
+	return m_fEta;
+}
+float BasicVecComponents::GetPhi() const
+{
+	return m_fPhi;
+}
+float BasicVecComponents::GetMass() const
+{
+	return m_fMass;
+}
+float BasicVecComponents::GetPx() const
+{
+	return m_fPx;
+}
+float BasicVecComponents::GetPy() const
+{
+	return m_fPy;
+}
+float BasicVecComponents::GetPz() const
+{
+	return m_fPz;
+}
+float BasicVecComponents::GetEn() const
+{
+	return m_fEn;
+}
+
--- SupportLibraries/Event_and_Particle_Kinematics/BasicVecComponents.h
+++ SupportLibraries/Event_and_Particle_Kinematics/BasicVecComponents.h
@@ -1,3 +1,5 @@
+#ifndef BASIC_VEC_COMPONENTS_H
+#define BASIC_VEC_COMPONENTS_H
 class TLorentzVector;
 
 class BasicVecComponents {
@@ -6,9 +8,21 @@ public:
     BasicVecComponents(TLorentzVector * pVecP4);
     void PrintVals(bool bPrintCartesian = true);
     void SetP4Vals(TLorentzVector * pVecP4);
+
+    float GetPt() const;
+    float GetEta() const;
+    float GetPhi() const;
+    float GetMass() const;
+    float GetPx() const;
+    float GetPy() const;
+    float GetPz() const;
+    float GetEn() const;
 private:
     float m_fPt, m_fEta, m_fPhi, m_fMass;
     float m_fPx, m_fPy, m_fPz, m_fEn;
 
     void UpdateParams(TLorentzVector * pVecP4);
 };
+
+
+#endif // BASIC_VEC_COMPONENTS_H
